de33a2c0ac35921150b0ba2d3238672eee51fc2b
Isizukuri <ragnarok49@gmail.com>
2015-11-17 17:23:28 +0200

    mess

--- livaravaTest2/settings.py
+++ livaravaTest2/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
@@ -100,3 +101,5 @@ USE_L10N = True
 
 USE_TZ = True
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
