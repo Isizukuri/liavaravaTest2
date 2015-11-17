c97d8b40e88b4af285b97c19943418850590808e
Isizukuri <ragnarok49@gmail.com>
2015-11-17 14:57:39 +0200

    Test commit

--- livaravaTest2/settings.py
+++ livaravaTest2/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 # Quick-start development settings - unsuitable for production
