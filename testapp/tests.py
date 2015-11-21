98751c6b41316adb1cef6e8c375955ba18ade3ec
Isizukuri <ragnarok49@gmail.com>
2015-11-21 21:04:20 +0200

    PEP8 problems fixed

--- testapp/tests.py
+++ testapp/tests.py
@@ -52,16 +52,17 @@ class TestWidgetPage(TestCase):
 
         self.assertTemplateUsed(response, 'widget.html')
 
-        #Checking if there is an element with help-text in rendered template to know, if it is properly rendered
+        # Checking if there is an element with help-text #
+        # in rendered template to know, if it is properly rendered #
         self.assertInHTML(
             u'<h3 class="panel-title">Вставте це в код своєї \
              сторінки і матимете рендомну текстову нотатку \
              з нашого тестового сайту!</h3>', response.content)
 
-        #Checking if link to widget is in rendered template.        
-        #It is impossible to do it in other way, because test client doesn`t give host url
+        # Checking if link to widget is in rendered template.
+        # It is impossible to do it in other way, because test client doesn`t
+        # give host url
         self.assertIn('widget_return', response.content)
-
 
 
 class TestWidgetReturn(TestCase, SimpleTestCase):
@@ -87,14 +88,14 @@ class TestWidgetReturn(TestCase, SimpleTestCase):
         self.url = reverse('widget_return')
 
     def test_widget_page(self):
-        #creating notes - list of "text" attributes of all TextNote objects
-        notes = [object.text for object in TextNote.objects.all()] 
+        # creating notes - list of "text" attributes of all TextNote objects
+        notes = [object.text for object in TextNote.objects.all()]
 
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
 
-        #Allocated random text note in response and checking if it is in notes
-        self.assertIn(unicode(response.content.split('<div>')[1].split('</div>')[0]), notes)
-
-
+        # Allocated random text note in response and checking if it is in notes
+        self.assertIn(
+            unicode(response.content.split('<div>')[1].split('</div>')[0]),
+            notes)
