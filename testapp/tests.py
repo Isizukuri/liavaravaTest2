1ff84f6a2d3ac1ae46afb6712c40e23bbe4726b5
Isizukuri <ragnarok49@gmail.com>
2015-11-21 20:05:02 +0200

    Widget tests improved

--- testapp/tests.py
+++ testapp/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase, SimpleTestCase, Client
 from django.core.urlresolvers import reverse
 
@@ -49,6 +50,19 @@ class TestWidgetPage(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        self.assertTemplateUsed(response, 'widget.html')
+
+        #Checking if there is an element with help-text in rendered template to know, if it is properly rendered
+        self.assertInHTML(
+            u'<h3 class="panel-title">Вставте це в код своєї \
+             сторінки і матимете рендомну текстову нотатку \
+             з нашого тестового сайту!</h3>', response.content)
+
+        #Checking if link to widget is in rendered template.        
+        #It is impossible to do it in other way, because test client doesn`t give host url
+        self.assertIn('widget_return', response.content)
+
+
 
 class TestWidgetReturn(TestCase, SimpleTestCase):
 
@@ -59,11 +73,28 @@ class TestWidgetReturn(TestCase, SimpleTestCase):
         TextNote.objects.get_or_create(
             text="Test Note for Testing 2")
 
+        TextNote.objects.get_or_create(
+            text="")
+
+        TextNote.objects.get_or_create(
+            text=u"Кирилична стрічка")
+
+        TextNote.objects.get_or_create(
+            text="abc" * 900)
+
         self.client = Client()
 
         self.url = reverse('widget_return')
 
     def test_widget_page(self):
+        #creating notes - list of "text" attributes of all TextNote objects
+        notes = [object.text for object in TextNote.objects.all()] 
+
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
+
+        #Allocated random text note in response and checking if it is in notes
+        self.assertIn(unicode(response.content.split('<div>')[1].split('</div>')[0]), notes)
+
+
